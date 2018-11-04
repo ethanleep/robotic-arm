5eee5b3f5272d28fef1f2187c31bc711d6e27dfd
ethanleep <20498042+ethanleep@users.noreply.github.com>
2018-11-03 22:34:14 -0500

    Added descriptions on the custom movement code

--- Custom_movement/d_custom.ino
+++ Custom_movement/d_custom.ino
@@ -1,4 +1,15 @@
-// Custom movement code goes here
+/* 
+In order to set what position an axis should be you must set the appropriate reqpos[n] variable for the proper axis.
+Ex. For axis two to position 200 the code would be reqpos[1] = 200;
+
+In order to make the appropriate axis start moving you must call the appropriate axis move function.
+Ex. For axis two to move to the previously defined position the code would be moving(2);
+
+Delays work as usual to add waits between movements.
+
+Custom movement code goes below in the loop statement.
+*/
+
 void loop() {
 
 }
