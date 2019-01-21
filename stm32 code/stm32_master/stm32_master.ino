1cd001baa1b37d96f5ff6dde53caf824fa5d6c9a
ethanleep <20498042+ethanleep@users.noreply.github.com>
2019-01-21 00:15:56 -0600

    Started on STM32 master/slave code

---
+++ stm32 code/stm32_master/stm32_master.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+int32_t bigNum = 12345678;
+void setup() {
+  // put your setup code here, to run once:
+Wire.begin();
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+byte myArray[4];
+
+myArray[0] = (bigNum >> 24) & 0xFF;
+myArray[1] = (bigNum >> 16) & 0xFF;
+myArray[2] = (bigNum >> 8) & 0xFF;
+myArray[3] = bigNum & 0xFF;
+
+Wire.beginTransmission(2);
+Wire.write('m');
+Serial.println("Sent m");
+Wire.write(myArray, 4);
+Wire.endTransmission();
+delay(250);
+}
---
+++ stm32 code/stm32_slave/stm32_slave.ino
@@ -0,0 +1,101 @@
+const char address = 2;
+
+#include <Wire_slave.h>
+
+const int r1 = PB14;
+const int r2 = PB15;
+volatile int pos = 0;
+int32_t reqpos = 0;
+const int motor1 = PB0;
+const int motor2 = PB1;
+
+void setup() {
+  pinMode(r1, INPUT_PULLUP);
+  pinMode(r2, INPUT_PULLUP);
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(r1), rot1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(r1), rot2, CHANGE);
+  Serial.begin(9600);
+  Wire.begin(address);
+  Wire.onReceive(com);
+
+}
+
+void loop() {
+  delay(250);
+}
+
+void com(int num) {
+  char z = Wire.read();
+  if(z == 'm') {
+    byte a,b,c,d;
+    a = Wire.read();
+    b = Wire.read();
+    c = Wire.read();
+    d = Wire.read();
+
+    reqpos = a;
+    reqpos = (reqpos << 8) | b;
+    reqpos = (reqpos << 8) | c;
+    reqpos = (reqpos << 8) | d;
+    moving();
+    Serial.print("movin` to ");
+    Serial.println(reqpos);
+  }
+}
+
+void moving() {
+  if(reqpos > pos) {
+    digitalWrite(motor1, HIGH);
+    digitalWrite(motor2, LOW);
+  }
+  else if (reqpos < pos) {
+    digitalWrite(motor1, LOW);
+    digitalWrite(motor2, HIGH);
+  }
+  else {
+    digitalWrite(motor1, LOW);
+    digitalWrite(motor2, LOW);
+  }
+}
+
+void rot1() {
+  if (digitalRead(r1) == HIGH) {
+    if (digitalRead(r2) == LOW) {
+      pos++;
+    }
+    else {
+      pos--;
+    }
+  }
+  else
+  {
+    if (digitalRead(r2) == HIGH) {
+      pos++;
+    }
+    else {
+      pos--;
+    }
+  }
+}
+
+void rot2() {
+  if (digitalRead(r2) == HIGH) {
+    if (digitalRead(r1) == HIGH) {
+      pos++;
+    }
+    else {
+      pos--;
+    }
+  }
+  else {
+    if (digitalRead(r1) == LOW) {
+      pos++;
+    }
+    else {
+      pos--;
+    }
+  }
+}
+
