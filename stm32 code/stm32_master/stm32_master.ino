0ddc5ced017cd2f9a61bf44ce03eb3cef2cb2cad
ethanleep <20498042+ethanleep@users.noreply.github.com>
2019-01-25 09:04:02 -0600

    Updated STM32 slave and master code

--- stm32 code/stm32_master/stm32_master.ino
+++ stm32 code/stm32_master/stm32_master.ino
@@ -1,24 +1,112 @@
 #include <Wire.h>
+
 int32_t bigNum = 12345678;
+const int led = PC13;
+int reqpos[6] = {0,0,0,0,0,0};
+int addresses[5] = {2,3,4,5,6};
+int serial = false;
+String mess;
+
 void setup() {
   // put your setup code here, to run once:
 Wire.begin();
 Serial.begin(9600);
+pinMode(led, OUTPUT);
 }
 
 void loop() {
+  noTone(led);
   // put your main code here, to run repeatedly:
+ // Store the serial data in a string
+  if (serial == false) {
+    mess = Serial.readString();
+  }
+  // Parse the lengths of the string and save it as a byte
+  byte len = mess.length();
+  // Check to make sure the string is longer than 0 characters
+  if (len > 0) {
+    // Save the first character in the string as a character
+    char fir = mess.charAt(0);
+    // Check to see if the first character matches any known prefixes
+    if (fir == 'M' or fir == 'm') {
+      // Save the motor number as a character
+      char numb = mess.charAt(1);
+      // This is doing some weird stuff to fix the motor numbers that are always off by 48, no idea why this works
+      int num = int(numb) - 48;
+      // Check to ensure the motor number given matches any known motors
+      if (num < 8 and num > -1) {
+        // This parsses the number after the colon, converts the data type, then sends it to the moving function and also sets the requested position varaible
+        String po = mess.substring(3);
+        int serpos = po.toInt();
+        reqpos[num] = serpos;
+        bigNum = serpos;
+        sendpos(num);
+      }
+      // If the motor number doesnt match known types then print an error
+      else {
+        Serial.println("Improper motor number. Please choose a number between 0 and 7. Type h or help for help");
+      }
+    }
+    // Check to see if the command sent is requesting help and if so, send a help string
+    else if (mess == "h" or mess == "H" or mess == "help" or mess == "Help") {
+      Serial.println("Type M followed by motor number, colon, and then position required");
+      Serial.println("Example: M2:500 would be motor 2 to position 500");
+    }
+    else if(fir == 'p' or fir == 'P') {
+      char numb = mess.charAt(1);
+      int num = int(numb) - 48;
+      requestpos(num);
+    }
+    // If the command doesnt match any known ones, then print an error
+    else {
+      Serial.println("Unknown command");
+    }
+    if (mess.indexOf('&') > 0) {
+      serial = true;
+      int del = mess.indexOf('&') + 2;
+      mess.remove(0, del);
+    }
+    else {
+      serial = false;
+    }
+  }
+}
+
+void sendpos(int adr) {
 byte myArray[4];
 
 myArray[0] = (bigNum >> 24) & 0xFF;
 myArray[1] = (bigNum >> 16) & 0xFF;
 myArray[2] = (bigNum >> 8) & 0xFF;
 myArray[3] = bigNum & 0xFF;
-
-Wire.beginTransmission(2);
+tone(led, 10);
+Wire.beginTransmission(adr);
 Wire.write('m');
-Serial.println("Sent m");
+Serial.print("Position ");
+Serial.print(bigNum);
+Serial.print(" sent to motor ");
+Serial.println(adr);
 Wire.write(myArray, 4);
 Wire.endTransmission();
 delay(250);
 }
+
+void requestpos(int adr) {
+  Wire.requestFrom(adr, 4);
+  int32_t pos;
+    byte a, b, c, d;
+    a = Wire.read();
+    b = Wire.read();
+    c = Wire.read();
+    d = Wire.read();
+
+    pos = a;
+    pos = (pos << 8) | b;
+    pos = (pos << 8) | c;
+    pos = (pos << 8) | d;
+    Serial.print("Motor ");
+    Serial.print(adr);
+    Serial.print(" is at position: ");
+    Serial.println(pos);
+}
+
--- stm32 code/stm32_slave/stm32_slave.ino
+++ stm32 code/stm32_slave/stm32_slave.ino
@@ -2,9 +2,10 @@ const char address = 2;
 
 #include <Wire_slave.h>
 
+const int led = PC13;
 const int r1 = PB14;
 const int r2 = PB15;
-volatile int pos = 0;
+volatile int32_t pos = 21412;
 int32_t reqpos = 0;
 const int motor1 = PB0;
 const int motor2 = PB1;
@@ -19,17 +20,20 @@ void setup() {
   Serial.begin(9600);
   Wire.begin(address);
   Wire.onReceive(com);
+  Wire.onRequest(posend);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
 
 }
 
 void loop() {
-  delay(250);
+  noTone(led);
 }
 
 void com(int num) {
   char z = Wire.read();
-  if(z == 'm') {
-    byte a,b,c,d;
+  if (z == 'm') {
+    byte a, b, c, d;
     a = Wire.read();
     b = Wire.read();
     c = Wire.read();
@@ -44,19 +48,31 @@ void com(int num) {
     Serial.println(reqpos);
   }
 }
+void posend() {
+  tone(led, 20);
+  byte myArray[4];
+  myArray[0] = (pos >> 24) & 0xFF;
+  myArray[1] = (pos >> 16) & 0xFF;
+  myArray[2] = (pos >> 8) & 0xFF;
+  myArray[3] = pos & 0xFF;
+  Wire.write(myArray, 4);
+}
 
 void moving() {
-  if(reqpos > pos) {
+  Wire.end();
+  tone(led, 3);
+  while (reqpos > pos) {
     digitalWrite(motor1, HIGH);
     digitalWrite(motor2, LOW);
   }
-  else if (reqpos < pos) {
+  while (reqpos < pos) {
     digitalWrite(motor1, LOW);
     digitalWrite(motor2, HIGH);
   }
-  else {
+  if (reqpos == pos) {
     digitalWrite(motor1, LOW);
     digitalWrite(motor2, LOW);
+    Wire.begin(address);
   }
 }
 
